b551d0831a5fd828bcb72bc00ef4a5f3bea8e563
francosta <francisco@fcosta.pt>
2020-05-29 23:04:00 +0100

    Added fonts and support for fonts.

--- App.js
+++ App.js
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+// FONTS
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'circular-standard': require('./assets/fonts/CircularStd-Book.ttf'),
+    'circular-bold': require('./assets/fonts/CircularStd-Bold.ttf'),
+  });
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -11,6 +22,19 @@ const styles = StyleSheet.create({
 });
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setFontLoaded(true);
+        }}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
