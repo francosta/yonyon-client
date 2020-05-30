72a3f045e0d80176134e34ea2510439cf04c5d3d
francosta <francisco@fcosta.pt>
2020-05-31 00:07:13 +0100

    dealt with env varibale issue.

--- src/api/yonyonApi.js
+++ src/api/yonyonApi.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { API_URL } from 'react-native-dotenv';
+// import { API_URL } from 'react-native-dotenv';
 
 export default axios.create({
   baseURL: 'http://a5039af9a2cc.ngrok.io',
