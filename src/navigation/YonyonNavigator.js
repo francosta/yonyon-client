4748bbd0c9a0b4c728f26db1724f75215780ba39
francosta <francisco@fcosta.pt>
2020-05-30 05:29:18 +0100

    Added screens and navigation files.

---
+++ src/navigation/YonyonNavigator.js
@@ -0,0 +1,6 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { Platform } from 'react-native';
+
+// Screen imports
