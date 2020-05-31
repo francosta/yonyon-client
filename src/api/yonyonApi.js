3db83a39905278c5ea6b9bc6f4ef86f80c7dfc68
Francisco <francisco@fcosta.pt>
2020-05-31 11:00:41 +0100

    Feature/yon redux setup (#9)
    * Added yon reducer and action for getting all yons. Yons being returned to state.?
    
    * Merged dev.

--- src/api/yonyonApi.js
+++ src/api/yonyonApi.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 // import { API_URL } from 'react-native-dotenv';
 
 export default axios.create({
-  baseURL: 'http://a5039af9a2cc.ngrok.io',
+  baseURL: 'http://55dc9f21f042.ngrok.io',
 });
--- src/screens/YonScreen/YonScreen.js
+++ src/screens/YonScreen/YonScreen.js
@@ -1,5 +1,12 @@
-import React from 'react';
+import React, {
+  useEffect,
+  useState,
+  useCallback,
+  ActivityIndicator,
+} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import * as yonActions from '../../store/actions/yon';
+import { useDispatch, useSelector } from 'react-redux';
 
 const styles = StyleSheet.create({
   screen: {
@@ -10,6 +17,49 @@ const styles = StyleSheet.create({
 });
 
 const YonScreen = (props) => {
+  const dispatch = useDispatch();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const [error, setError] = useState();
+  const yons = useSelector((state) => state.yon.yons);
+
+  const loadProducts = useCallback(async () => {
+    setError(null);
+    setIsRefreshing(true);
+    try {
+      await dispatch(yonActions.getYons());
+    } catch (err) {
+      setError(err.message);
+    }
+    setIsRefreshing(false);
+  }, [dispatch, setIsLoading, setError]);
+
+  useEffect(() => {
+    // const willFocusSub = props.navigation.addListener(
+    //   'willFocus',
+    //   loadProducts
+    // );
+    // return () => {
+    //   willFocusSub.remove();
+    // };
+  }, [loadProducts]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    loadProducts().then(() => {
+      setIsLoading(false);
+    });
+  }, [dispatch, loadProducts]);
+
+  // if (isLoading) {
+  //   return (
+  //     <View style={styles.centered}>
+  //       <ActivityIndicator size="large" color="red" />
+  //     </View>
+  //   );
+  // }
+
   return (
     <View style={styles.screen}>
       <Text>This is the YonScreen</Text>
