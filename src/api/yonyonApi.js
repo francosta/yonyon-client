ca332d1c06d0cda2ca6dea375b782d0c81691b05
francosta <francisco@fcosta.pt>
2020-06-04 07:05:30 +0100

    Added dotenv and API url.

--- src/api/yonyonApi.js
+++ src/api/yonyonApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-// import { API_URL } from 'react-native-dotenv';
+import { API_URL } from 'react-native-dotenv';
 
 export default axios.create({
-  baseURL: 'http://97d1c6f4e94b.ngrok.io',
+  baseURL: API_URL,
 });
